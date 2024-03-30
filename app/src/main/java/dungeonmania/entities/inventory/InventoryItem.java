53ec1ce0dc4c084e90352e2f9201933b5e0581a2
Sameen Chowdhury <sameenchowdhury@Sameens-MacBook-Air.local>
2024-03-31 03:45:02 +1100

    Task-1D fix

--- app/src/main/java/dungeonmania/entities/inventory/InventoryItem.java
+++ app/src/main/java/dungeonmania/entities/inventory/InventoryItem.java
@@ -1,9 +1,8 @@
 package dungeonmania.entities.inventory;
 
-import dungeonmania.entities.Overlappable;
 /**
  * A marker interface for InventoryItem
  */
-public interface InventoryItem extends Overlappable {
+public interface InventoryItem {
     
 }
